3a102f634820bf64689b4fa3a3b056ed95033229
Salem Thompson <sthompson@lenovo.wsrixnet.local>
2012-07-23 14:53:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '8c2d94b705e335288b2bb8942b367089c596da6e8a93595d9f8dd717207ca706bdeec9aa61fd9935498857e2a49bbdf0b9144ebeda3cb7c76a4aafed8096a366'
